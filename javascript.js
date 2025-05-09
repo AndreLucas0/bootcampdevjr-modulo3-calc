7a2c66fb0a7503be113798e4f4b7b249cff205f8
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-05-09 17:13:56 -0300

    tentando fazer do jeito certo na func calc

--- javascript.js
+++ javascript.js
@@ -1,8 +1,60 @@
 function calc(){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    const b1 = document.getElementById("b1ID");
+    const b2 = document.getElementById("b2ID");
+    const b3 = document.getElementById("b3ID");
+    const b4 = document.getElementById("b4ID");
+
+
+    b1.addEventListener("click", function(event) {
+        if (event.currentTarget.id === "b1ID"){
+            var output = num1 + num2;
+
+            document.getElementById("output").value = output;  
+        } if (event.currentTarget.id === "b2ID"){
+            var output = num1 - num2;
+
+            document.getElementById("output").value = output;  
+        } if (event.currentTarget.id === "b3ID"){
+            var output = num1 * num2;
+
+            document.getElementById("output").value = output;  
+        } if (event.currentTarget.id === "b4ID"){
+            var output = num1 / num2;
+
+            document.getElementById("output").value = output;  
+        }
+    })
     
-    var output = num1 + num2;
+    
+}
+
+/*
+function calcSub(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    
+    var output = num1 - num2;
 
     document.getElementById("output").value = output;
 }
+
+function calcMult(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    
+    var output = num1 * num2;
+
+    document.getElementById("output").value = output;
+}
+
+function calcDiv(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    
+    var output = num1 / num2;
+
+    document.getElementById("output").value = output;
+}
+    */
