29e4d5e8835feb06b4772c383c0dc6479dbbacce
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-05-10 10:07:42 -0300

    closes #4 - implementar as 4 operações matemáticas com js

--- javascript.js
+++ javascript.js
@@ -1,33 +1,16 @@
-function calc(){
+function calc(op){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
-    const b1 = document.getElementById("b1ID");
-    const b2 = document.getElementById("b2ID");
-    const b3 = document.getElementById("b3ID");
-    const b4 = document.getElementById("b4ID");
+    var output = 0;
 
-
-    b1.addEventListener("click", function(event) {
-        if (event.currentTarget.id === "b1ID"){
-            var output = num1 + num2;
-
-            document.getElementById("output").value = output;  
-        } if (event.currentTarget.id === "b2ID"){
-            var output = num1 - num2;
-
-            document.getElementById("output").value = output;  
-        } if (event.currentTarget.id === "b3ID"){
-            var output = num1 * num2;
-
-            document.getElementById("output").value = output;  
-        } if (event.currentTarget.id === "b4ID"){
-            var output = num1 / num2;
-
-            document.getElementById("output").value = output;  
-        }
-    })
+    switch(op) {
+        case '+' : output = num1 + num2; break;
+        case '-' : output = num1 - num2; break;
+        case '*' : output = num1 * num2; break;
+        case '/' : output = num1 / num2; break;
+    }
     
-    
+    document.getElementById("output").value = output;
 }
 
 /*
